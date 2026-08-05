9c111fa35c809819faa981294f22bebdbc614a3e
Tomas Skorvan <tomas.skorvan@gmail.com>
2026-08-05 07:49:23 +0200

    Guard the FullCalendar api before resizing

--- client/src/app/issue/components/issue-calendar/issue-calendar.component.ts
+++ client/src/app/issue/components/issue-calendar/issue-calendar.component.ts
@@ -176,7 +176,7 @@ export class IssueCalendarComponent implements AfterViewInit, OnDestroy {
     constructor() {
         effect(() => {
             this.showFilter();
-            queueMicrotask(() => this.calendarRef()?.getApi().updateSize());
+            queueMicrotask(() => this.calendarRef()?.getApi()?.updateSize());
         });
     }
 
